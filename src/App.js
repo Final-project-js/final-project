c4036ae2c938086e2d003015287811207081de37
Orozobek <oroz1111omurkulov@gmail.com>
2021-08-06 12:59:17 +0600

    tst

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import "./App.less";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { SearchPart } from "./components/SearchPart";
 import { Products } from "./components/Products";
-import "./App.less";
-// import Splide from "./components/TopCateg";
 import { TopCateg } from "./components/TopCateg";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <SearchPart />
       <Products /> 
       <TopCateg/>
-      {/* <Splide/>  */}
        <Footer />
     </div>
   );
