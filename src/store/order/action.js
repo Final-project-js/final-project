ca485232f7cd7705bbdba53d3171bfa05fc7ba6f
Orozobek <oroz1111omurkulov@gmail.com>
2021-08-11 17:35:53 +0600

    created order branch

--- src/store/order/action.js
+++ src/store/order/action.js
@@ -1,1 +1,14 @@
-export const
+export const ADD_ORDER = 'ADD_ORDER'
+export const DELETE_ORDER = 'DELETE_ORDER'
+
+
+export const add_order = (obj) => ({
+    type: ADD_ORDER,
+    obj
+})
+
+export const delete_order = (obj, id) => ({
+    type: DELETE_ORDER,
+    obj,
+    id
+})
---
+++ src/store/order/reducer.js
@@ -0,0 +1,73 @@
+import { ADD_ORDER, DELETE_ORDER } from './action.js'
+
+const initialState = {
+    orders: [],
+    totalCount: 0,
+    totalPrice: 0
+}
+
+export const orderReducer = (state = initialState, action) => {
+    console.log(action)
+
+    switch(action.type) {
+
+        case ADD_ORDER:
+            let arr = [...state.orders]
+            let is_ch = state.orders.findIndex((item)=> {
+                return item.title === action.obj.title
+            }) 
+            // console.log(is_ch);
+            if(is_ch === -1) {
+                return {
+                    ...state,
+                    orders: [...state.orders, action.obj],
+                    totalCount: state.totalCount + 1,
+                    totalPrice: state.totalPrice + action.obj.price
+    
+                }
+            } else {
+                return {
+                    orders: state.orders.map((el, id )=> {
+                        return arr[is_ch].title === el.title
+                        ? {...el, count: el.count + 1, price: el.price + el.num}
+                        : el
+                    }),
+                    totalCount: state.totalCount + 1,
+                    totalPrice: state.totalPrice + action.obj.price
+                }
+            }
+
+        case DELETE_ORDER:
+            let arr1 = [...state.orders]
+            let is_ch1 = state.orders.findIndex((item)=> {
+                return item.title === action.obj.title
+            })
+            if(action.obj.count !== 1) {
+
+            return {
+                ...state,
+                orders: state.orders.map((el, id)=> {
+                    return arr1[is_ch1].title === el.title
+                    ? {...el, count: el.count -1, price: el.price + el.num}
+                    : el
+                }),
+                totalCount: state.totalCount -1,
+                totalPrice: state.totalPrice - action.obj.num
+            } }
+                
+            else {
+                    return {
+                        ...state,
+                        orders: state.orders.filter((_, id)=> {
+                            return id !== action.id
+                        }),
+                        totalCount: state.totalCount -1,
+                totalPrice: state.totalPrice - action.obj.num
+                        
+                    }
+            }
+            
+        default:
+            return state
+    }
+}
